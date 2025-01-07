5f0531ed04771083e3ed0027130bdc2f25a29ef5
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2025-01-07 17:29:21 +0900

    [fix] delete mysql password

--- src/main/java/com/example/demo/service/general/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.service.general;/*
-package com.example.demo.service.permit;
-
-public class PermitUserService {
-}
-*/
--- src/main/java/com/example/demo/service/permit/impl/PermitUserServiceImpl.java
+++
@@ -1,6 +0,0 @@
-/*
-package com.example.demo.service.permit.impl;
-
-public class PermitUserServiceImpl {
-}
-*/
