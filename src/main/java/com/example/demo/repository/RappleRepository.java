70372242784fc70cf1eb62c5941417d04616aa33
ajwoong <sosloahn@gmail.com>
2025-01-11 20:01:49 +0900

    [FEAT]: 홈 화면 조회 (마감임박순 조회, 래플 둘러보기 응모자순으로 조회)

--- src/main/java/com/example/demo/repository/RappleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Rapple;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RappleRepository extends JpaRepository<Rapple, Long> {
-}
--- src/main/java/com/example/demo/service/HomeService.java
+++ src/main/java/com/example/demo/service/HomeService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
 
-import com.example.demo.domain.converter.RappleConverter;
+import com.example.demo.domain.converter.RaffleConverter;
 import com.example.demo.domain.dto.HomeResponseDTO;
-import com.example.demo.entity.Rapple;
-import com.example.demo.repository.RappleRepository;
+import com.example.demo.entity.Raffle;
+import com.example.demo.repository.RaffleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,45 +12,46 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HomeService {
 
-    private final RappleRepository rappleRepository;
+    private final RaffleRepository raffleRepository;
 
     public HomeResponseDTO getHome(){
 
         HomeResponseDTO result = new HomeResponseDTO();
-        List<Rapple> rapples = rappleRepository.findAll();
+        List<Raffle> rapples = raffleRepository.findAll();
 
 
         // 마감임박인 래플 5개 조회
         LocalDateTime now = LocalDateTime.now();
 
-        List<Rapple> rapplesSortedByEndAt = rapples.stream()
+        List<Raffle> rafflesSortedByEndAt = rapples.stream()
                 .filter(r -> Duration.between(now, r.getEndAt()).toMillis() >= 0)
                 .sorted(Comparator.comparingLong(r -> Duration.between(now, r.getEndAt()).toMillis()))
                 .limit(5)
                 .toList();
 
-        for (Rapple rapple : rapplesSortedByEndAt) {
-            HomeResponseDTO.RappleDTO rappleDTO = RappleConverter.toRappleDTO(rapple);
-            result.getApproaching().add(rappleDTO);
+        for (Raffle raffle : rafflesSortedByEndAt) {
+            HomeResponseDTO.RaffleDTO raffleDTO = RaffleConverter.toRaffleDTO(raffle);
+            result.getApproaching().add(raffleDTO);
         }
 
         // 응모자순으로 래플 조회
-        Stream<Rapple> rapplesSortedByApplyList = rapples.stream()
+        List<Raffle> rafflesSortedByApplyList = rapples.stream()
                 .sorted((r1, r2) -> Integer.compare(
                         r2.getApplyList() != null ? r2.getApplyList().size() : 0,
                         r1.getApplyList() != null ? r1.getApplyList().size() : 0
-                ));
+                ))
+                .toList();
 
-        for (Rapple rapple : rapplesSortedByEndAt) {
-            HomeResponseDTO.RappleDTO rappleDTO = RappleConverter.toRappleDTO(rapple);
-            result.getRapples().add(rappleDTO);
+
+        for (Raffle raffle : rafflesSortedByApplyList) {
+            HomeResponseDTO.RaffleDTO raffleDTO = RaffleConverter.toRaffleDTO(raffle);
+            result.getRaffles().add(raffleDTO);
         }
 
         //TODO 내가 찜한 래플 조회 (로그인이 됐을 시)
