37223540dae306ff37194af39503fde9a059f38f
limdodod <rosalim2001@naver.com>
2025-01-11 16:50:47 +0900

    찜 기능 추가

--- src/main/java/com/example/demo/entity/Like.java
+++ src/main/java/com/example/demo/entity/Like.java
@@ -8,6 +8,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(name = "likes")
 public class Like extends BaseEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
