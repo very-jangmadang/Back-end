da25324016a7b278f9f5c595f60310209643bbe2
limdodod <rosalim2001@naver.com>
2025-01-12 20:56:49 +0900

    review

--- src/main/java/com/example/demo/controller/general/ReviewController.java
+++ src/main/java/com/example/demo/controller/general/ReviewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("permit/review")
+@RequestMapping("api/permit/review")
 public class ReviewController {
 
     private final ReviewRepository reviewRepository;
