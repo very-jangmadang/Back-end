7d6269ae89fb8540c79686072220076c867fddfd
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2025-01-12 17:50:15 +0900

    [fix] api/permit/ 컨트롤러 승인

--- src/main/java/com/example/demo/controller/general/HomeController.java
+++ src/main/java/com/example/demo/controller/general/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/permit/home")
 @RequiredArgsConstructor
 public class HomeController {
 
