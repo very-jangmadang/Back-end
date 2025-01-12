2e1fb1e1bcc75db5b6960f50ced088f8ceab13c3
ajwoong <sosloahn@gmail.com>
2025-01-12 11:56:48 +0900

    [FEAT]: 홈 화면 조회 (마감임박순 조회, 래플 둘러보기 응모자순으로 조회), 파일 정렬

--- src/main/java/com/example/demo/controller/general/HomeController.java
+++ src/main/java/com/example/demo/controller/general/HomeController.java
@@ -1,16 +1,15 @@
-package com.example.demo.controller;
+package com.example.demo.controller.general;
 
 import com.example.demo.base.ApiResponse;
+import com.example.demo.base.status.SuccessStatus;
 import com.example.demo.domain.dto.HomeResponseDTO;
-import com.example.demo.service.HomeService;
+import com.example.demo.service.general.HomeService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.example.demo.base.status.SuccessStatus._OK;
 
 @RestController
 @RequestMapping("/home")
@@ -23,7 +22,7 @@ public class HomeController {
     @GetMapping("")
     public ApiResponse<HomeResponseDTO> home(){
         HomeResponseDTO result = homeService.getHome();
-        return ApiResponse.of(_OK, result);
+        return ApiResponse.of(SuccessStatus._OK, result);
     }
 
 }
