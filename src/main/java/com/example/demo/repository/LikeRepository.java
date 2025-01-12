484a6e1930537b313821cb5c99dfd845a63c80aa
limdodod <rosalim2001@naver.com>
2025-01-12 16:34:47 +0900

    likes

--- src/main/java/com/example/demo/repository/LikeRepository.java
+++ src/main/java/com/example/demo/repository/LikeRepository.java
@@ -1,18 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Like;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
-public interface likeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, Long> {
 
     // 특정 사용자가 찜한 raffleId 목록 조회
     List<Like> findByUserId(Long userId);
