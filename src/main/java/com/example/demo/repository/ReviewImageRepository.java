082cb1bbff08ae367960f9b8b26561a0cd7525d1
limdodod <rosalim2001@naver.com>
2025-01-20 01:33:27 +0900

    like

--- src/main/java/com/example/demo/repository/ReviewImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Review;
-import com.example.demo.entity.ReviewImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
-}
