c85bdade74b91ea2ce7c536b7bb37c6a0b5fc919
Yeohuiju <yeohuiju03@gmail.com>
2025-01-21 12:49:14 +0900

    [FEAT] 래플 응모 조건 추가

--- src/main/java/com/example/demo/repository/ApplyRepository.java
+++ src/main/java/com/example/demo/repository/ApplyRepository.java
@@ -2,11 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Apply;
 import com.example.demo.entity.Raffle;
+import com.example.demo.entity.Raffle;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    boolean existsByRaffleAndUser(Raffle raffle, User user);
 
     int countByRaffle(Raffle raffle);
 
