e1355d645ced2c20467721899d3f3c6687fd76d3
Yeohuiju <yeohuiju03@gmail.com>
2025-01-23 11:45:12 +0900

    [FEAT] 래플 결과 조회

--- src/main/java/com/example/demo/repository/AddressRepository.java
+++ src/main/java/com/example/demo/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Address;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Optional<Address> findById(Long id);
+
+    Optional<Address> findByIdAndUser(Long addressId, User user);
 }
