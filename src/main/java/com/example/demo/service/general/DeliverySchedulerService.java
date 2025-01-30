0e28afb7f2ee059371720dad695ae93df14486fd
Yeohuiju <yeohuiju03@gmail.com>
2025-01-31 06:22:55 +0900

    [FEAT] delivery Job 설정

---
+++ src/main/java/com/example/demo/service/general/DeliverySchedulerService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.general;
+
+import com.example.demo.entity.Delivery;
+import org.quartz.SchedulerException;
+
+public interface DeliverySchedulerService {
+
+    void scheduleDeliveryJob(Delivery delivery) throws SchedulerException;
+}
