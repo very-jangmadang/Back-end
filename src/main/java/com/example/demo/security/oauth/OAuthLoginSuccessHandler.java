a2bfaf4d05d4d01b0c07769ee8ee2f7a8dad8e01
parkhyungjin <gud5223@naver.com>
2025-02-03 01:09:51 +0900

    fix : 리다이렉트 주소 수정 #31

--- src/main/java/com/example/demo/security/oauth/OAuthLoginSuccessHandler.java
+++ src/main/java/com/example/demo/security/oauth/OAuthLoginSuccessHandler.java
@@ -42,12 +42,12 @@ public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHand
         String email = kakaoAccount.get("email").toString();
         log.info("{}", kakaoAccount.get("email"));
 
-        String redirectUrl = "https://www.jangmadang.site/";
+        String redirectUrl = "https://www.jangmadang.site";
 
         // 기존 회원이 아닌경우
         if (!userService.isExistUser(email)) {
             userService.createUser(email);
-            redirectUrl = "https://www.jangmadang.site/nickname";
+            redirectUrl = "https://www.jangmadang.site/kakao";
         }
 
         // 엑세스 토큰 생성
