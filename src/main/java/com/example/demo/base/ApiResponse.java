f023249ee52454a661c227d35cf68a18bca2a983
신지윤 (Yoon) <108733746+Yoon0221@users.noreply.github.com>
2025-02-14 11:05:47 +0900

    Update ApiResponse.java

--- src/main/java/com/example/demo/base/ApiResponse.java
+++ src/main/java/com/example/demo/base/ApiResponse.java
@@ -25,6 +25,6 @@ public class ApiResponse<T> {
     }
 
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
-        return new ApiResponse<>(false, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), result);
+        return new ApiResponse<>(false, "400", code.getReasonHttpStatus().getMessage(), result);
     }
 }
