cd4b84def76c9d928c0912cd8ae863446ff8d61c
신지윤 (Yoon) <108733746+Yoon0221@users.noreply.github.com>
2025-02-14 11:31:36 +0900

    Update CustomException.java

--- src/main/java/com/example/demo/base/code/exception/CustomException.java
+++ src/main/java/com/example/demo/base/code/exception/CustomException.java
@@ -7,28 +7,14 @@ import org.springframework.http.ResponseEntity;
 public class CustomException extends RuntimeException {
 
     private final ErrorStatus errorStatus;
-    private final Object data;
 
     public CustomException(ErrorStatus errorStatus) {
         super(errorStatus.getMessage());
         this.errorStatus = errorStatus;
-        this.data = null;
     }
-
-    public CustomException(ErrorStatus errorStatus, Object data) {
-        super(errorStatus.getMessage());
-        this.errorStatus = errorStatus;
-        this.data = data;
-    }
-
     public ErrorStatus getErrorStatus() {
         return errorStatus;
     }
-
-    public Object getData() {
-        return data;
-    }
-
     public static <T> ResponseEntity<ApiResponse<T>> createErrorResponse(ErrorStatus errorStatus, T data) {
         return ResponseEntity
                 .status(errorStatus.getHttpStatus())
