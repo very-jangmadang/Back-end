16694e3fa9a5b5c20e6318c3bb748fcfdb28c0d8
신지윤 (Yoon) <108733746+Yoon0221@users.noreply.github.com>
2025-02-14 11:47:22 +0900

    Update ApiResponse.java

--- src/main/java/com/example/demo/base/ApiResponse.java
+++ src/main/java/com/example/demo/base/ApiResponse.java
@@ -29,7 +29,7 @@ public class ApiResponse<T> {
     }
 
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
-        return new ApiResponse<>(false, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), result);
+        throw new CustomException(ErrorStatus.COMMON_UNAUTHORIZED);
     }
 
 }
