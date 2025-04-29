faedf68041a566bcc8f307ef863e077302dcfdfd
ajwoong <sosloahn@gmail.com>
2025-04-29 21:07:03 +0900

    [Fix]: 재배포

--- src/main/java/com/example/demo/service/general/impl/HomeServiceImpl.java
+++ src/main/java/com/example/demo/service/general/impl/HomeServiceImpl.java
@@ -38,6 +38,7 @@ public class HomeServiceImpl implements HomeService {
 
     @Override
     public HomeResponseDTO getHome(int page, int size) {
+
         // 마감임박인 래플 5개 조회
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime maxTime = now.plusHours(24);
