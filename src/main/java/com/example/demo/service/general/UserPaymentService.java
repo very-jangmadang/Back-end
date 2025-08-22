688407f164f8954ef5aeb6dbdff4d21a5e9c5b52
nonsmileCheetah <pgw31111@gmail.com>
2025-08-22 23:15:10 +0900

    feat : 티켓 구매 내역 히스토리 vary 코인 버전에 맞기 변경

--- src/main/java/com/example/demo/service/general/UserPaymentService.java
+++ src/main/java/com/example/demo/service/general/UserPaymentService.java
@@ -2,6 +2,7 @@ package com.example.demo.service.general;
 
 import com.example.demo.base.ApiResponse;
 import com.example.demo.domain.dto.Payment.*;
+import com.example.demo.domain.dto.TopUp.TopUpResponse;
 
 import java.util.List;
 
@@ -9,6 +10,6 @@ public interface UserPaymentService {
     ApiResponse<UserTicketResponse> getUserTickets(Long userId);
     ApiResponse<UserBankInfoResponse> postUserPaymentInfo(Long userId, UserBankInfoRequest userBankInfoRequest);
     ApiResponse<UserBankInfoResponse> getUserPaymentInfo(Long userId);
-    ApiResponse<List<PaymentResponse>> getPaymentHistory(Long userId, String period);
+    ApiResponse<List<TopUpResponse>> getTopUpHistory(Long userId, String period);
     ApiResponse<Void> tradeTickets(Long userId, String role, int ticketCount);
 }
